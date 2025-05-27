67046276a8c74f55db64ad92be58f6318fdf1999
ghostflyby <ghostflyby+git@outlook.com>
2025-05-27 16:40:30 +0800

    fix: ignore symbols directly inside interfaces

--- compiler-plugin/src/com/ghostflyby/kotlin/plugin/fir/DefaultInternalTransformer.kt
+++ compiler-plugin/src/com/ghostflyby/kotlin/plugin/fir/DefaultInternalTransformer.kt
@@ -2,22 +2,34 @@ package com.ghostflyby.kotlin.plugin.fir
 
 import org.jetbrains.kotlin.descriptors.Visibilities
 import org.jetbrains.kotlin.fir.FirSession
+import org.jetbrains.kotlin.fir.analysis.checkers.getContainingClassSymbol
 import org.jetbrains.kotlin.fir.analysis.checkers.hasModifier
 import org.jetbrains.kotlin.fir.copyWithNewDefaults
 import org.jetbrains.kotlin.fir.declarations.*
+import org.jetbrains.kotlin.fir.declarations.utils.isInterface
 import org.jetbrains.kotlin.fir.declarations.utils.visibility
 import org.jetbrains.kotlin.fir.extensions.FirStatusTransformerExtension
+import org.jetbrains.kotlin.fir.symbols.SymbolInternals
 import org.jetbrains.kotlin.fir.symbols.impl.FirClassLikeSymbol
 import org.jetbrains.kotlin.lexer.KtTokens
 
 class DefaultInternalTransformer(session: FirSession) : FirStatusTransformerExtension(session) {
-  override fun needTransformStatus(declaration: FirDeclaration): Boolean =
-    declaration is FirMemberDeclaration &&
-        declaration.run {
-          origin == FirDeclarationOrigin.Source &&
-              !symbol.hasModifier(KtTokens.PUBLIC_KEYWORD)
-              && (visibility == Visibilities.Unknown || visibility == Visibilities.Public)
-        }
+  @OptIn(SymbolInternals::class)
+  override fun needTransformStatus(declaration: FirDeclaration): Boolean {
+    if (declaration !is FirMemberDeclaration)
+      return false
+
+    val containingClass = declaration.getContainingClassSymbol()?.fir
+    if (containingClass is FirRegularClass && containingClass.isInterface) {
+      return false
+    }
+
+    return declaration.run {
+      origin == FirDeclarationOrigin.Source &&
+          !hasModifier(KtTokens.PUBLIC_KEYWORD)
+          && (visibility == Visibilities.Unknown || visibility == Visibilities.Public)
+    }
+  }
 
   override fun transformStatus(status: FirDeclarationStatus, declaration: FirDeclaration): FirDeclarationStatus {
     return status.copyWithNewDefaults(
